33f550f6e7bf49cf9292b837cfcc29f87ab7a264
donlieu95 <34259296+donlieu95@users.noreply.github.com>
2021-04-08 20:46:58 -0500

    Password prompt only
    First draft prompts for a password and spits it out in plaintext

---
+++ Project2.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+#include <bits/stdc++.h>
+#include <fstream> 
+#include <string> 
+#include <cstring>  
+#include <cstdio>
+#include <iomanip>
+#include <cstdlib>
+#include <cmath>
+#include <sys/time.h>
+#include <time.h>
+
+using namespace std;
+
+int main ()
+{
+	string password;
+	cout << "Please enter your password.  Spaces are not allowed." << endl;
+	cin >> password;
+	cout << "Password in plaintext is:  " << password << endl;
+}
